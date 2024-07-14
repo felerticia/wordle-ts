21b47ac50e23a8d8649d4bfb94e030c534c2b64f
Amir Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-07-14 12:46:37 +0200

    5- Handle key strokes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { pickRandomWord } from "./helpers/helpers";
 import { ROWS } from "./constants/contants";
@@ -8,12 +8,47 @@ import Keyboard from "./components/Keyboard/Keyboard";
 function App() {
   const [solution, setSolution] = useState("");
   const [guesses, setGuesses] = useState<string[]>(new Array(ROWS).fill(""));
+  const [currentWord, setCurrentWord] = useState("");
+  const [currentRow, setCurrentRow] = useState(0);
 
   const selectWord = () => setSolution(pickRandomWord);
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    console.log(e);
-  };
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      const key = e.key.toLowerCase();
+      if (key === "backspace") {
+        if (currentWord.length) {
+          setCurrentWord((currentWord) => currentWord.slice(0, -1));
+        }
+        return;
+      }
+
+      if (currentWord.length < 5 && key === "enter") {
+        return;
+      }
+
+      if (currentWord.length === 5) {
+        if (key !== "enter") {
+          return;
+        } else {
+          setGuesses((guesses) =>
+            guesses.map((guess, idx) =>
+              idx === currentRow ? currentWord : guess
+            )
+          );
+          setCurrentRow((currentRow) => currentRow + 1);
+          setCurrentWord("");
+          return;
+        }
+      }
+
+      if (key >= "a" && key <= "z") {
+        setCurrentWord((currentWord) => currentWord + key.toUpperCase());
+        return;
+      }
+    },
+    [currentWord, currentRow]
+  );
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyDown);
